b55a37299421b191a2bc00a4734845fa9f247d79
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-04 15:48:49 +0600

    server work

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const allData = require("./data/chef.json");
+const cors = require("cors");
 
-app.get('/', (req, res) =>{
-    res.send('chef is here');
+app.use(cors());
 
-});
 
 app.listen(port, () =>{
     console.log(`chef is running on port: ${port}`)
 })
+
+app.get('/', (req, res)=>{
+    res.send("hello world");
+})
+
+
+app.get('/allData', (req, res) =>{
+    res.send(allData);
+});
+
+
+
+
 
