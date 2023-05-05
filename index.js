67e86ce6b2a4b357e57d7087183f50eb587551fe
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-05 11:09:01 +0600

    json edit

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const allData = require("./data/chef.json");
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
 
-app.use(cors());
+const chef = require('./data/chef.json')
 
+app.use(cors())
 
-app.listen(port, () =>{
-    console.log(`chef is running on port: ${port}`)
+app.get('/',(req,res) =>{
+    res.send('chef client is running')
 })
 
-app.get('/', (req, res)=>{
-    res.send("hello world");
+app.get('/chef',(req,res) =>{
+    res.send(chef)
 })
 
-
-app.get('/allData', (req, res) =>{
-    res.send(allData);
-});
-
-app.get('/allData/:id', (req, res) => {
+app.get('/chef/:id',(req,res) => {
     const id =req.params.id
     console.log(id)
     if( id ==0)
     {
-        res.send(allData)
+        res.send(chef)
     }else{
-        const chefData = allData.find(n => n.id == id)
+        const chefData = chef.find(n => n.id == id)
         res.send(chefData)
     }
    
     console.log(chefData)
-
-    
 })
 
-
-
-
-
+app.listen(port,() =>{
+    console.log(`Chef Server API is running on port : ${port}`)
+})
